94172b7155db9230509cf5dc093fb75bd50f2ca4
pedram <pedram.post@gmail.com>
2020-11-18 13:53:29 -0800

    add new useForm hoos

---
+++ src/components/shared/form-components/input2.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./input2.scss";
+
+const Input2 = (props) => {
+  return (
+    <div>
+      <label></label>
+      <input />
+    </div>
+  );
+};
+
+export default Input2;
