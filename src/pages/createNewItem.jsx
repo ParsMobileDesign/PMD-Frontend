640d13ab7f1148611be9b6daf119e65ba7ba791c
pedram <pedram.post@gmail.com>
2020-11-18 13:11:09 -0800

    asd

--- src/pages/createNewItem.jsx
+++ src/pages/createNewItem.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Button from "../components/shared/form-components/Button";
-import Input3 from '../components/shared/form-components/input3'
+import Input3 from "../components/shared/form-components/input3";
 import Input from "../components/shared/form-components/input";
-import { useForm } from "../components/shared/Util/form-hook";
+import { useForm } from "../components/shared/Util/form-hook-2";
 
 import "./createNewItem.scss";
 
@@ -16,46 +16,44 @@ const CreateNewItem = () => {
       description: {
         value: "",
         isValid: false,
-      },
+      }
     },
     isFormValid: false,
   });
+
   const submitHandler = (event) => {
     event.preventDefault();
     console.log(formState);
   };
-  const onClick = () => {
-    console.log("click button 2");
-  };
   return (
     <div className="container">
       <div className="row">
-        <Input
+        <Input3
           element="input"
           id="title"
           type="text"
           placeholder="Title"
           onInput={inputHandler}
+          cssClass="form-control"
         />
       </div>
       <div className="row">
-        <Input
+        <Input3
           element="input"
           id="description"
           type="text"
           placeholder="description"
           onInput={inputHandler}
+          cssClass="form-control"
         />
       </div>
-
       <Button
         title="Save"
         clickHandler={submitHandler}
         type="submit"
         cssClass="btn btn-info"
-        disabled={!formState.isFormValid}
+        // disabled={!formState.isFormValid}
       />
-      <Input3 />
     </div>
   );
 };
